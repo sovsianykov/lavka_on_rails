dd8a16e3c617952013739a32a92ae32bb4e8244b
sovsianykov <swebproger@gmail.com>
2023-08-02 10:24:06 +0200

    InfoSevice added

--- app/controllers/api/v1/bands_controller.rb
+++ app/controllers/api/v1/bands_controller.rb
@@ -1,19 +1,16 @@
 class Api::V1::BandsController < ApplicationController
   before_action :find_band, only: %i[ show edit update destroy ]
 
-  # GET /bands or /bands.json
   def index
     @bands = Band.all
     render json: @bands
   end
 
-  # GET /bands/1 or /bands/1.json
   def show
     @band_with_musicians = InfoService.show_band_with_musicians(@band)
     render json: @band_with_musicians
   end
 
-  # POST /bands or /bands.json
   def create
     @band = Band.new(band_params)
 
@@ -24,7 +21,6 @@ class Api::V1::BandsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /bands/1 or /bands/1.json
   def update
     if @band.update(band_params)
       render json: @band
@@ -33,19 +29,16 @@ class Api::V1::BandsController < ApplicationController
     end
   end
 
-  # DELETE /bands/1 or /bands/1.json
   def destroy
     @band.destroy
     head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_band
       @band = Band.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def band_params
       params.require(:band).permit(:band_name)
     end
